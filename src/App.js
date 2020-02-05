ed29862fd1be507855b74e8771131278b1dfc7c9
parkhyogeun <hyo92@namver.com>
2020-02-05 11:41:53 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+ render(){
+   return(
+    <div>
+      <h1>hello park</h1>
+      <li>첫번째</li>
+      <li>두번째</li>
+      <li>세번째</li>
     </div>
-  );
+   );
+ }  
 }
 
 export default App;
